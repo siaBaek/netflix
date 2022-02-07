f3cefb985f947cb9bffd586dc024fafc34cd2e48
lazySia <tldk9640@naver.com>
2022-02-08 02:07:07 +0900

    atom update

--- src/atom.ts
+++ src/atom.ts
@@ -1,19 +1,16 @@
 import { atom } from "recoil";
 
-/**
- * Movie Detail of atom
- */
 export const isDetail = atom({
   key: "movieDetail",
   default: false,
 });
 
-export const isTvDetail = atom({
-  key: "tvDetail",
-  default: false,
-});
+// export const isTvDetail = atom({
+//   key: "tvDetail",
+//   default: false,
+// });
 
-export const GlobalSearchClose = atom({
-  key: "searchClose",
-  default: false,
-});
+// export const GlobalSearchClose = atom({
+//   key: "searchClose",
+//   default: false,
+// });
